d494df01f06bfe8f96cc7ae4f98a738d588d00df
jeroen-manders <jeroen.manders@external.mckinsey.com>
2019-02-06 11:00:44 +0100

    wip

--- provider.tf
+++ provider.tf
@@ -22,9 +22,9 @@ provider "aws" {
 
 # aws credentials
 
-provider "aws" {
+#provider "aws" {
 
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region = "eu-west-1"
-}
+  #access_key = "${var.aws_access_key}"
+  #secret_key = "${var.aws_secret_key}"
+ # region = "eu-west-1"
+#}
